fc8f96aa76c0526d70585590c6a103dc679f4f14
Chris_Velázquez04 <placebo0704@gmail.com>
2022-11-10 14:54:54 +0000

    server express

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 //** === === Backend APIREST ExpressJs === ===  */
-console.log('Hello World');
+
+const express = require(`express`);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running in Port http://localhost:${PORT}...`);
+});
